f8e2ee8ecd7c803fb53b3dd3e9559e4d935fc9cb
Ioan Zicu <ioan.zicu94@gmail.com>
2020-11-02 19:25:47 +0100

    add appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 import React from "react";
-import "./App.css";
-import Form from "./components/Form";
 import Container from "@material-ui/core/Container";
+import AppBar from "@material-ui/core/AppBar";
+import LocalShippingIcon from "@material-ui/icons/LocalShipping";
+
 import SeachByLocation from "./components/SearchByLocation";
+import Form from "./components/Form";
+import "./App.css";
 
 function App() {
   return (
-    <Container maxWidth="md">
-      <div className="App">
-        <h1>Epic Route Finder Front End</h1>
-        <hr />
-        <Form />
-        <hr />
-        <SeachByLocation />
-      </div>
-    </Container>
+    <div>
+      <AppBar position="static">
+        <h1 style={{ textAlign: "center" }}>
+          <LocalShippingIcon
+            style={{ marginBottom: "-0.5rem" }}
+            fontSize="large"
+          />{" "}
+          Route Finder
+        </h1>
+      </AppBar>
+
+      <Container maxWidth="md">
+        <div className="App">
+          <hr />
+          <Form />
+          <hr />
+          <SeachByLocation />
+        </div>
+      </Container>
+    </div>
   );
 }
 

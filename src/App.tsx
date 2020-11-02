71be7b433c358ddd2aa3f261a0ff78ea160271c9
Ioan Zicu <ioan.zicu94@gmail.com>
2020-11-02 19:46:14 +0100

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 import LocalShippingIcon from "@material-ui/icons/LocalShipping";
 
 import SeachByLocation from "./components/SearchByLocation";
@@ -9,25 +11,31 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <AppBar position="static">
         <h1 style={{ textAlign: "center" }}>
           <LocalShippingIcon
-            style={{ marginBottom: "-0.5rem" }}
+            style={{ marginBottom: "-0.5rem", paddingRight: "1rem" }}
             fontSize="large"
-          />{" "}
+          />
           Route Finder
         </h1>
       </AppBar>
 
       <Container maxWidth="md">
-        <div className="App">
-          <hr />
-          <Form />
-          <hr />
-          <SeachByLocation />
-        </div>
+        <br />
+        <Form />
+        <hr />
+        <SeachByLocation />
       </Container>
+
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="body1" color="inherit">
+            &copy; 2020
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
